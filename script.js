17793c20978d4f2b4e90e96e8502b3e210ca6d06
daewoonie <alamulhuda307@gmail.com>
2026-01-22 10:43:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -788,7 +788,7 @@ let galaxyAudio = null;
 
 function preloadGalaxyAudio() {
   const audioSources = [
-   "https://youtu.be/rtOvBOTyX00?si=JzHujc0OHPE1VLIe"
+   "https://www.youtube.com/watch?v=d4OMqGKBl6E&list=RDd4OMqGKBl6E&start_radio=1&ab_channel=ARS"
   ];
 
   const randomIndex = Math.floor(Math.random() * audioSources.length);
